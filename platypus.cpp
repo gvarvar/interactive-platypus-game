32144697368bc4dbf9030fb66d6752e0e3e31d05
gvarvar <83052919+gvarvar@users.noreply.github.com>
2024-10-17 09:26:43 +1100

    the game is now to create a board, since we can find one in reasonable time.

--- platypus.cpp
+++ platypus.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <vector>
 #include <cstdio>
+#include <functional>
 
 enum Animal
 {
@@ -97,7 +98,7 @@ class PlatypusGame
         for (int i=0; i<boardSize; i++) board[i]=YELLOW;
         animal_strings[KANGAROO] = "Kangaroo";
         animal_strings[WOMBAT] = "Wombat";
-        animal_strings[EMU] = "Emu";
+        animal_strings[EMU] = "Emu\t";
         animal_strings[PLATYPUS] = "Platypus";
 
         animal_emoji[KANGAROO] = "🦘";
@@ -106,22 +107,34 @@ class PlatypusGame
         animal_emoji[PLATYPUS] = "🦫";
     }
 
-    void play()
+    void setboard(const std::string_view& str)
     {
-        std::cout << "Playing platypus game\n";
+        for (int i=0; i<21; ++i)
+        {
+            board[i] = (Colour)str[i];
+        }
+    }
 
-        for (int move = 0; machine.getAnimal() != TERMINATED; ++move)
+    int play(bool print=false)
+    {
+        //std::cout << "Playing platypus game\n";
+
+        for (int move = 0; machine.getAnimal() != TERMINATED && move<500; ++move)
         {
             const int pos = machine.getPosition() % 20; // Loops
-            printf("%*s%s\n", pos*2, "",animal_emoji[machine.getAnimal()].c_str());
+
             // Print the board
-            for (Colour c : board)
+            if (print)
             {
-                std::cout << (c==YELLOW ? "🟨" : "🟩");
+                printf("%*s%s\n", pos*2, "",animal_emoji[machine.getAnimal()].c_str());
+                for (Colour c : board)
+                {
+                    std::cout << (c==YELLOW ? "🟨" : "🟩");
+                }
+                std::cout<<" | Animal: \"" << animal_strings[machine.getAnimal()] << "\"   \t| Points: " << points << " | Move: " << move <<'\n';
+                // Print a position marker
+                printf("%*s☝️\n\n", pos*2, "");
             }
-            std::cout<<" | Animal: \"" << animal_strings[machine.getAnimal()] << "\"   \t| Points: " << points << " | Move: " << move <<'\n';
-            // Print a position marker
-            printf("%*s☝️\n\n", pos*2, "");
 
             // Do the move
             Colour colour = machine.doTurn(board[pos]);
@@ -129,12 +142,23 @@ class PlatypusGame
 
             if (machine.getAnimal() == TERMINATED)
             {
-                std::cout << "Machine terminated\n";
-                return;
+                //std::cout << "Machine terminated in " << move << " moves\n";
+                return move;
             }
         }
+        return -1;
     }
 };
+
+void generateStrings(std::string current, int remaining,const std::function<void(const std::string&)>& process) {
+    if (remaining == 0) {
+        process(current);
+        return;
+    }
+    
+    generateStrings(current + 'Y', remaining - 1,process);
+    generateStrings(current + 'G', remaining - 1,process);
+}
 
 int main()
 {
@@ -152,11 +176,32 @@ int main()
     trans[Transition(PLATYPUS,YELLOW)] = Action(KANGAROO,GREEN,WATTLE);
 
     // platypus(1, [t(y,k,g,e,gg),t(g,k,y,w,wa),t(y,e,y,p,gg),t(g,e,y,k,gg),t(y,w,g,w,gg),t(g,w,g,w,gg),t(y,p,g,k,wa)]).
-
+    int min = 99999999;
+    std::string min_string;
+    int duplicates = 0;
     PlatypusMachine machine(trans);
+    generateStrings("",21,[&min,&min_string,&machine,&duplicates](const std::string& s) {
+        PlatypusGame game(machine);
+        game.setboard(s);
+        const int m = game.play();
+        if (m < min)
+        {
+            min = m;
+            min_string = s;
+            duplicates = 0;
+        }
+        if (m == min) duplicates++;
+    });
+
+    std::cout << "best answer is " << min_string << " in " << min << " with " << duplicates << " other solutions\n";
+
+    //PlatypusMachine machine(trans);
 
     PlatypusGame game(machine);
-    game.play();
+    game.setboard("GGGYYYYGYGGGGGG");
+    const int f = game.play(true);
+
+    std::cout << "You ended the platypus game in " << f << " moves.\n";
 
     return 0;
 }
